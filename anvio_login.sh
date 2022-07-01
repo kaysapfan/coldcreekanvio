a7d4906f222ddbdc7ec13f3386c727e327bb3f15
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-01 11:25:17 -0700

    new bash script as an intro

--- anvio_login.sh
+++ anvio_login.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+qrsh -l h_rt=03:00:00,h_data=10G    # this will login to a shell on the hoffman2 server
+# please note that the amount of data may have to be increased, this is just a basic setting
+
+module load anaconda3   # this will allow conda to open anvio
+
+conda activate anvio-7.1
+
+"Anvi'o is now open, you can run your next command now."
+
+# from here you can run whatever script you want- probably start with a file
+
