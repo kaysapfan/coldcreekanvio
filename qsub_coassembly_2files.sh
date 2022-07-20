7938f449f2c14c7b234034e56e49d2ddef1aa794
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-20 10:33:34 -0700

    smaller files

--- qsub_coassembly_2files.sh
+++ qsub_coassembly_2files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#$ -N qsub_coassembly_25G_2files
-#$ -o /u/scratch/b/big2258/coldcreekanvio/coassembly_files/2sample_files.1133
-#$ -e /u/scratch/b/big2258/coldcreekanvio/coassembly_files/2sample_files.1133
+#$ -N qsub_coassembly_2files
+#$ -o /u/scratch/b/big2258/coldcreekanvio/coassembly_files/2sample_files.1031
+#$ -e /u/scratch/b/big2258/coldcreekanvio/coassembly_files/2sample_files.1031
 #$ -cwd 
 #$ -l h_data=6.25G
 #$ -l h_rt=23:00:00
@@ -32,13 +32,13 @@ MIN_CONTIG_SIZE=1000
 
 # create two environment variables and look at them
 #CHANGE THE FILES/TRANSFER AND THEN CORRECT HTE 01_QC HERE
-R1s=`ls 01_QC/2sample_ex/*QUALITY_PASSED_R1* | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
-R2s=`ls 01_QC/2sample_ex/*QUALITY_PASSED_R2* | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
+R1s=`ls coldcreekanvio/E11_1_1.fq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
+R2s=`ls coldcreekanvio/E11_1_2.fq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
     # already created the environment variables- keep here to make sure it runs in qsub job
 
 # run megahit
 # specify what min contig size and num threads (depends on cpu)- check server cpu
-megahit -1 $R1s -2 $R2s --min-contig-len $MIN_CONTIG_SIZE -m 0.85 -o 02_ASSEMBLY.mini.1133/ -t $NUM_THREADS
+megahit -1 $R1s -2 $R2s --min-contig-len $MIN_CONTIG_SIZE -m 0.85 -o 02_ASSEMBLY.mini.1031/ -t $NUM_THREADS
     # the 1000 min contig size and 40 num threads is the default from anvio
 
 
