0101c0b6c1d399bdee1060a12f632891fba0736f
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-25 14:52:16 -0700

    create contigs db and run hmms

--- qsub_run-hmms.sh
+++ qsub_run-hmms.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#$ -N run-hmms_7.25-E1.1
-#$ -o /u/scratch/b/big2258/coldcreekanvio/hmms/7.25-1.1
+#$ -N run-hmms_7.25-E12.1
+#$ -o /u/scratch/b/big2258/coldcreekanvio/hmms/7.25-12.1o
 
 #TODO: change output and error files
 
-#$ -e /u/scratch/b/big2258/coldcreekanvio/hmms/7.25_1.1
+#$ -e /u/scratch/b/big2258/coldcreekanvio/hmms/7.25_12.1e
 #$ -cwd 
 #$ -l h_data=6.25G
 #$ -l h_rt=23:00:00
@@ -20,6 +20,17 @@
 module load anaconda3
 
 conda activate anvio-7.1
+
+# issues with format- reformat the fasta bc of bad deflines
+    # http://merenlab.org/2016/06/22/anvio-tutorial-v2/#take-a-look-at-your-fasta-file
+anvi-script-reformat-fasta /u/scratch/b/big2258/coldcreekanvio/02_ASSEMBLY/7.21_E12.1/final.contigs.fa -o E12_1.contigs-fixed.fa -l 0 --simplify-names --report-file
+# report file creates a tab-delim file where bad 
+
+mv E12_1.contigs-fixed.fa E12_1.contigs.fa
+# overwrite the name so it makes it easier below
+
+# create a contigs db for each of the samples before running the hmms
+ anvi-gen-contigs-database -f E12_1.contigs.fa -o E12_1.contigs.db -n 'Sample E12_2'
 
 # I need the end flag bc I tried to run it from the command line and it took too long
 # if there are some hmm files the program won't run, this will overwrite them.
