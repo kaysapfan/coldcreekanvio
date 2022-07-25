727b3b855daff7bb145d6820794553c288c14766
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-24 20:29:50 -0700

    13.3

--- qsub_coassembly_2files.sh
+++ qsub_coassembly_2files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#$ -N qsub_coassembly_7.24_E1.3
-#$ -o /u/scratch/b/big2258/coldcreekanvio/coassembly_files/onesamp_7.24_1.3
+#$ -N qsub_coassembly_7.24_E13.3
+#$ -o /u/scratch/b/big2258/coldcreekanvio/coassembly_files/onesamp_7.24_13.3
 
 #TODO: change output and error files
 
-#$ -e /u/scratch/b/big2258/coldcreekanvio/coassembly_files/onesamp_7.24_1.3
+#$ -e /u/scratch/b/big2258/coldcreekanvio/coassembly_files/onesamp_7.24_13.3
 #$ -cwd 
 #$ -l h_data=6.25G
 #$ -l h_rt=23:00:00
@@ -38,13 +38,13 @@ KMAX_SIZE=59
 
 # create two environment variables and look at them
 #CHANGE THE FILES/TRANSFER AND THEN CORRECT HTE 01_QC HERE
-R1s=`ls 01_QC/sample_E1_3-QUALITY_PASSED_R1.fastq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
-R2s=`ls 01_QC/sample_E1_3-QUALITY_PASSED_R2.fastq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
+R1s=`ls 01_QC/sample_E13_3-QUALITY_PASSED_R1.fastq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
+R2s=`ls 01_QC/sample_E13_3-QUALITY_PASSED_R2.fastq | python -c 'import sys; print(",".join([x.strip() for x in sys.stdin.readlines()]))'`
     # already created the environment variables- keep here to make sure it runs in qsub job
 
 # run megahit
 # specify what min contig size and num threads (depends on cpu)- check server cpu
-megahit -1 $R1s -2 $R2s --k-max $KMAX_SIZE --min-contig-len $MIN_CONTIG_SIZE -m 0.85 -o 02_ASSEMBLY/7.21_E1.3 -t $NUM_THREADS
+megahit -1 $R1s -2 $R2s --k-max $KMAX_SIZE --min-contig-len $MIN_CONTIG_SIZE -m 0.85 -o 02_ASSEMBLY/7.21_E13.3 -t $NUM_THREADS
     # the 1000 min contig size and 40 num threads is the default from anvio
 
 #TODO: change kmax value to 59 instead of 81
