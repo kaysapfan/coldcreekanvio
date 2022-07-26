8a0d4aa65910ff18258a0c595ecebda1429ec747
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-26 10:47:24 -0700

    11.1

--- qsub_gencontigsdb.sh
+++ qsub_gencontigsdb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#$ -N run-hmms_7.25-E12.1
-#$ -o /u/scratch/b/big2258/coldcreekanvio/hmms/7.25-12.1o
+#$ -N run-hmms_7.26-E11.1
+#$ -o /u/scratch/b/big2258/coldcreekanvio/hmms/7.26_11.1o
 
 #TODO: change output and error files
 
-#$ -e /u/scratch/b/big2258/coldcreekanvio/hmms/7.25_12.1e
+#$ -e /u/scratch/b/big2258/coldcreekanvio/hmms/7.26_11.1e
 #$ -cwd 
 #$ -l h_data=6.25G
 #$ -l h_rt=23:00:00
@@ -21,16 +21,20 @@ module load anaconda3
 
 conda activate anvio-7.1
 
+# FROM HERE TO THE NEXT CAP I DID BEFOREHAND 
+
 # issues with format- reformat the fasta bc of bad deflines
     # http://merenlab.org/2016/06/22/anvio-tutorial-v2/#take-a-look-at-your-fasta-file
-anvi-script-reformat-fasta /u/scratch/b/big2258/coldcreekanvio/02_ASSEMBLY/7.21_E12.1/final.contigs.fa -o E12_1.contigs-fixed.fa -l 0 --simplify-names 
+#anvi-script-reformat-fasta /u/scratch/b/big2258/coldcreekanvio/02_ASSEMBLY/7.21_E12.1/final.contigs.fa -o E12_1.contigs-fixed.fa -l 0 --simplify-names 
 #  '--report-file' will create a tab-delim file where bad deflines are changed... I did not use this 
 
-mv E12_1.contigs-fixed.fa E12_1.contigs.fa
+#mv E12_1.contigs-fixed.fa E12_1.contigs.fa
 # overwrite the name so it makes it easier below
 
+# DID THE ABOVE ALREADY 
+
 # create a contigs db for each of the samples before running the hmms
- anvi-gen-contigs-database -f E12_1.contigs.fa -o E12_1.contigs.db -n 'Sample E12_2'
+ anvi-gen-contigs-database -f E11_1.contigs.fa -o E11_1.contigs.db -n 'Sample E12_2'
 
     # Anvi'o will use 'prodigal' by Hyatt et al (doi:10.1186/1471-2105-11-119) to
     # identify open reading frames in your data.
