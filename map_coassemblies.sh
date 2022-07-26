42c79b68ded5b1b2bc20b687dc89aecf62ab6c32
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-25 17:07:32 -0700

    mapping updated

--- map_coassemblies.sh
+++ map_coassemblies.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
+#$ -N map_7.25-E1.1
+#$ -o /u/scratch/b/big2258/coldcreekanvio/map_files/7.25-1.1o
+
+#TODO: change output and error files
+
+#$ -e /u/scratch/b/big2258/coldcreekanvio/map_files/7.25_1.1e
+#$ -cwd 
+#$ -l h_data=6.25G
+#$ -l h_rt=23:00:00
+#$ -l h_vmem=25G
+#$ -pe shared 4
+#$ -w e
+    # verify options and abort if there is an error
+#$ -m ea 
+#$ -M pfannmulk@sou.edu
 
 
-#$ -N mapping_coassemblies
-#$ -wd <set wd>
-#$ -m ea -M pfannmulk@sou.edu
+. /u/local/Modules/default/init/modules.sh
+module load anaconda3
 
-# make a directory for the mapped reads
-mkdir 04_MAPPING 
+conda activate anvio-7.1
+
+# make a directory for the mapped reads - did this beforehand
+#mkdir 04_MAPPING 
+
+# initialize variables here:
+NUM_THREADS=4
 
 # build an index for the contigs
-bowtie2-build 03_CONTIGS/contigs.fa 04_MAPPING/contigs
+bowtie2-build /u/scratch/b/big2258/coldcreekanvio/02_ASSEMBLY/7.21_onesample/final.contigs.fa 04_MAPPING/E1_1.contigs
 
 # create/get an indexed BAM file for one of the samples
-bowtie2 --threads $NUM_THREADS -x 04_MAPPING/contigs -1 01_QC/Sample_01-QUALITY_PASSED_R1.fastq -2 01_QC/Sample_01-QUALITY_PASSED_R2.fastq -S 04_MAPPING/Sample_01.sam
-samtools view -F 4 -bS 04_MAPPING/Sample_01.sam > 04_MAPPING/Sample_01-RAW.bam
-anvi-init-bam 04_MAPPING/Sample_01-RAW.bam -o 04_MAPPING/Sample_01.bam
-rm 04_MAPPING/Sample_01.sam 04_MAPPING/Sample_01-RAW.bam
-# replace numthreads and usually you use 8 (need an integer but depends on cpu)
+bowtie2 --threads $NUM_THREADS -x 04_MAPPING/E1_1.contigs -1 01_QC/sample_E1_1-QUALITY_PASSED_R1.fastq -2 01_QC/sample_E1_1-QUALITY_PASSED_R2.fastq -S 04_MAPPING/E1_1.sam
+samtools view -F 4 -bS 04_MAPPING/E1_1.sam > 04_MAPPING/E1_1-RAW.bam
+anvi-init-bam 04_MAPPING/E1_1-RAW.bam -o 04_MAPPING/E1_1.bam
+rm 04_MAPPING/E1_1.sam 04_MAPPING/E1_1-RAW.bam
 
-# write a for loop text with the above example ...
 
 
 
