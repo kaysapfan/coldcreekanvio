c0b3c95b13b89f64e3ae9b042d71f6e507a74584
Kaysa Pfannmuller <pfannmulk@sou.edu>
2022-07-27 09:45:00 -0700

    rerun profile

--- qsub_createprofiledb.sh
+++ qsub_createprofiledb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#$ -N profiledb_7.26-E1.1-5.30
-#$ -o /u/scratch/b/big2258/coldcreekanvio/profile_files/7.26_1.1_5.30o
+#$ -N profiledb_7.27-E1.1
+#$ -o /u/scratch/b/big2258/coldcreekanvio/profile_files/7.27o
 
 #TODO: change output and error files
 
-#$ -e /u/scratch/b/big2258/coldcreekanvio/profile_files/7.26_1.1_5.30e
+#$ -e /u/scratch/b/big2258/coldcreekanvio/profile_files/7.27e
 #$ -cwd 
 #$ -l h_data=6.25G
 #$ -l h_rt=23:00:00
@@ -22,6 +22,6 @@ module load anaconda3
 conda activate anvio-7.1
 
 
-anvi-profile -i /u/scratch/b/big2258/coldcreekanvio/04_MAPPING/E1_1.bam -c E1_1.contigs.db -o /u/scratch/b/big2258/coldcreekanvio/cc.profiles/E1_1.530.PROFILE.db
+anvi-profile -i /u/scratch/b/big2258/coldcreekanvio/04_MAPPING/E1_1.bam -c E1_1.contigs.db -o /u/scratch/b/big2258/coldcreekanvio/E1_1.PROFILE.db
 
 # create a profile from sample [...] bam and contig.dbs to output 
